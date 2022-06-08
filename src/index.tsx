09deb9c555ec4cc2133140ebe17225bc1673f102
PhongVan <fmlphongvan@gmail.com>
2022-06-09 01:11:15 +0700

    init commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { CssBaseline } from "@mui/material";
+import ReactDOM from "react-dom";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById("root")!;
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<CssBaseline />
@@ -20,7 +18,7 @@ root.render(
 			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>
-);
+	, container);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
